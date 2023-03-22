c414d0ae072ac770e116d5e35a062d7ef751d1e1
emil237 <marianatalaat1@gmail.com>
2023-03-22 08:26:39 -0700

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -8,102 +8,48 @@
 # ###########################################
 
 ###########################################
-TMPDIR='/tmp'
-PACKAGE="enigma2-plugin-extensions-freeserver"
+TEMPATH=/tmp
+OPKGINSTALL="opkg install --force-overwrite"
+MY_IPK="enigma2-plugin-extensions-freeserver_8.2.0_all.ipk"
+MY_DEB="enigma2-plugin-extensions-freeserver_8.2.0_all.deb"
 MY_URL="https://raw.githubusercontent.com/emil237/freeserver/main"
-PYTHON_VERSION=$(python -c"import sys; print(sys.version_info.major)")
-
-#########################
-if [ -f /etc/opkg/opkg.conf ]; then
-    STATUS='/var/lib/opkg/status'
-    OSTYPE='Opensource'
-    OPKG='opkg update'
-    OPKGINSTAL='opkg install'
-    OPKGLIST='opkg list-installed'
-    OPKGREMOV='opkg remove --force-depends'
-elif [ -f /etc/apt/apt.conf ]; then
+if [ -f /etc/apt/apt.conf ] ; then
     STATUS='/var/lib/dpkg/status'
-    OSTYPE='DreamOS'
-    OPKG='apt-get update'
-    OPKGINSTAL='apt-get install'
-    OPKGLIST='apt-get list-installed'
-    OPKGREMOV='apt-get purge --auto-remove'
-    DPKINSTALL='dpkg -i --force-overwrite'
+    OS='DreamOS'
+elif [ -f /etc/opkg/opkg.conf ] ; then
+   STATUS='/var/lib/opkg/status'
+   OS='Opensource'
 fi
 
-#########################
-VERSION=$(wget $MY_URL/version -qO- | grep 'version' | cut -d "=" -f2-)
-GIT=$(wget $MY_URL/version -qO- | grep 'commit' | cut -d "=" -f2-)
-CHECK_VERSION=$($OPKGLIST $PACKAGE | cut -d'+' -f2 | awk '{ print $1 }')
-rm -rf $TMPDIR/"${PACKAGE:?}"* >/dev/null 2>&1
+# remove old version #
+opkg remove enigma2-plugin-extensions-freeserver
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/FreeServer
 
-#########################
-if [ "$CHECK_VERSION" = "$VERSION" ]; then
-    echo " You are use the laste Version: $VERSION"
-    exit 1
-elif [ -z "$CHECK_VERSION" ]; then
-    echo
-    clear
-else
-    $OPKGREMOV $PACKAGE
-fi
-
-########################
-install() {
-    if grep -qs "Package: $1" $STATUS; then
-        echo
-    else
-        $OPKG >/dev/null 2>&1
-        echo "   >>>>   Need to install $1   <<<<"
-        echo
-        $OPKGINSTAL "$1"
-        sleep 1
-        clear
-    fi
-}
-
-########################
-if [ "$PYTHON_VERSION" -eq 3 ]; then
-    for i in python3-codecs python3-core python3-json python3-netclient; do
-        install $i
-    done
-else
-    for i in python-codecs python-core python-json python-netclient; do
-        install $i
-    done
-    if [ $OSTYPE = "DreamOS" ]; then
-        for d in gstreamer1.0-plugins-base-meta gstreamer1.0-plugins-good-spectrum; do
-            install $d
-        done
-    fi
-fi
-
-########################
-echo "Insallling FreeServer plugin Please Wait ......"
-if [ $OSTYPE = "Opensource" ]; then
-    wget $MY_URL/${PACKAGE}_${VERSION}_all.ipk -qP $TMPDIR
-    $OPKGINSTAL $TMPDIR/${PACKAGE}_${VERSION}_all.ipk
-else
-    wget $MY_URL/${PACKAGE}_${VERSION}_all.deb -qP $TMPDIR
-    $DPKINSTALL $TMPDIR/${PACKAGE}_${VERSION}_all.deb
-    $OPKGINSTAL -f -y
-fi
-
-########################
-rm -rf $TMPDIR/"${PACKAGE:?}"*
 echo ""
-echo "***********************************************************************"
-echo "**                                                                    *"
-echo "**                    FreeServer    : $VERSION                            *"
-echo "**                       Uploaded by: Emil_Nabil                     *"
-echo "**                                                                    *"
-echo "***********************************************************************"
-echo ""
-
-if [ $OSTYPE = "Opensource" ]; then
-    killall -9 enigma2
-else
-    systemctl restart enigma2
+# Download and install plugin
+cd /tmp
+set -e
+ if which dpkg > /dev/null 2>&1; then
+  wget "$MY_URL/$MY_DEB"
+		dpkg -i --force-overwrite $MY_DEB; apt-get install -f -y
+wait
+rm -f $MY_DEB
+	else
+  wget "$MY_URL/$MY_IPK"
+		$OPKGINSTALL $MY_IPK
+wait
+rm -f $MY_IPK
+	fi
+echo "================================="
+set +e
+cd ..
+wait
+	if [ $? -eq 0 ]; then
+echo ">>>>  SUCCESSFULLY INSTALLED <<<<"
 fi
-
+		echo "********************************************************************************"
+echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
+sleep 4;
+echo "#########################################################"
+sleep 2
 exit 0
